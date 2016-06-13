59313792b0c9e5f0092a53e02182599d5eac6376
Le Tuan Anh <anhlt1983@gmail.com>
2016-06-13 15:05:36 +0700

    prepare data before processing

--- app/src/main/java/com/packtpub/masteringopencvandroid/featureapp/Camera.java
+++ app/src/main/java/com/packtpub/masteringopencvandroid/featureapp/Camera.java
@@ -1,20 +1,66 @@
 package com.packtpub.masteringopencvandroid.featureapp;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+import java.io.IOException;
 
 public class Camera extends Activity {
 
     public final static String TAG = "FeatureApp::Camera";
+
+    private final int SELECT_PHOTO = 0;
+
+    private Mat originalMat;
+    private Bitmap currentBitmap;   // bitmap to be applied with different algorithm
+    private Bitmap originalBitmap;  // store original bitmap for compare
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            Log.d(TAG, "onManagerConnected is called");
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i("OpenCV Status", "OpenCV loaded successfully");
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate is called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        // Initializing OpenCV Package Manager
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
     }
 
     @Override
@@ -27,7 +73,120 @@ public class Camera extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.open_gallery) {
+            Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse("content://media/internal/images/media"));
+            startActivityForResult(intent, SELECT_PHOTO);
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK && intent != null) {
+            Uri selectedImage = intent.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Log.d(TAG, "Length of file path " + filePathColumn.length);
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn,
+                    null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String filePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Log.d(TAG, "filePath " + filePath);
+
+            // To speed up loading of image
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 2;
+
+            Bitmap temp = BitmapFactory.decodeFile(filePath, options);
+
+            // Get orientation information
+            int orientation = 0;
+
+            try {
+                ExifInterface imgParams = new ExifInterface(filePath);
+                orientation = imgParams.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //Rotating the image to get the correct orientation
+            Matrix rotate90 = new Matrix();
+            rotate90.postRotate(orientation);
+            originalBitmap = rotateBitmap(temp, orientation);
+
+            //Convert Bitmap to Mat
+            Bitmap tempBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+            originalMat = new Mat(tempBitmap.getHeight(), tempBitmap.getWidth(), CvType.CV_8U);
+            Utils.bitmapToMat(tempBitmap, originalMat);
+
+            currentBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, false);
+            loadImageToImageView();
+        }
+    }
+
+    private void loadImageToImageView() {
+        ImageView imgView = (ImageView) findViewById(R.id.image_view);
+        imgView.setImageBitmap(currentBitmap);
+    }
+
+    //Function to rotate bitmap according to image parameters
+    public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
+
+        Matrix matrix = new Matrix();
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_NORMAL:
+                return bitmap;
+            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+                matrix.setScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate(180);
+                break;
+            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+                matrix.setRotate(180);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_TRANSPOSE:
+                matrix.setRotate(90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_TRANSVERSE:
+                matrix.setRotate(-90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                matrix.setRotate(-90);
+                break;
+            default:
+                return bitmap;
+        }
+        try {
+            Bitmap bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            bitmap.recycle();
+            return bmRotated;
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
 }
