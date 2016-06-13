58e36ec01b6bb4088cf44874421fe7aaeb754090
Le Tuan Anh <anhlt1983@gmail.com>
2016-06-13 14:06:36 +0700

    add menu list

--- app/src/main/java/com/packtpub/masteringopencvandroid/featureapp/Camera.java
+++ app/src/main/java/com/packtpub/masteringopencvandroid/featureapp/Camera.java
@@ -1,13 +1,33 @@
 package com.packtpub.masteringopencvandroid.featureapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class Camera extends AppCompatActivity {
+public class Camera extends Activity {
+
+    public final static String TAG = "FeatureApp::Camera";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate is called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu
+        getMenuInflater().inflate(R.menu.filename, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
